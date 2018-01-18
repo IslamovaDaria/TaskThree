3341a6195ba37cf750d67ffef9b2d133670fed72
Daria <islamovaida@yandex.ru>
2018-01-18 21:41:37 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThree
+{
+    class Program
+    {
+        static double ReadDouble()
+        {
+            double num = 0;
+            bool OK = false;
+            do
+            {
+                try
+                {
+                    num = Convert.ToDouble(Console.ReadLine());
+                    OK = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка ввода. Ожидалось число.");
+                    OK = false;
+                }
+            } while (!OK);
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            double x, y;
+            bool answer = false;
+            Console.WriteLine("Введите x:");
+            x = ReadDouble();
+            Console.WriteLine("Введите y:"); 
+            y = ReadDouble();
+            answer = (y >= Math.Abs(x)) || (y >= 1);
+            if (answer) Console.WriteLine("Точка принадлежит заданной области.");
+            else Console.WriteLine("Точка не принадлежит заданной области.");
+
+            Console.ReadLine();
+        }
+    }
+}
